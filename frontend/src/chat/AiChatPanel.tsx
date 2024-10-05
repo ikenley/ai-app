57f76554ce62787478e7aa00663c92208431e7db
ikenley <ikenley6@gmail.com>
2024-10-05 15:43:47 -0400

    assistant-ui

---
+++ frontend/src/chat/AiChatPanel.tsx
@@ -0,0 +1,13 @@
+import { Thread } from "@assistant-ui/react";
+
+/** Open source chat UI.
+ * Extends https://www.assistant-ui.com/docs/ui/styled/Thread */
+const AiChatPanel = () => {
+  return (
+    <div className="ai-chat-panel">
+      <Thread />
+    </div>
+  );
+};
+
+export default AiChatPanel;
--- frontend/src/chat/ChatPage.tsx
+++ frontend/src/chat/ChatPage.tsx
@@ -1,24 +1,30 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import "@assistant-ui/react/styles/index.css";
 import Navbar from "../shared/Navbar";
 import ChatPanel from "./ChatPanel";
+import { MyRuntimeProvider } from "./MyRuntimeProvider";
+import AiChatPanel from "./AiChatPanel";
 
 const ChatPage = () => {
   return (
     <div className="chat-page">
-      <Navbar />
-      <Container maxWidth="sm" component="main" sx={{ mt: 2 }}>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          component="p"
-          sx={{ mb: 1 }}
-        >
-          Chat with an AI bot
-        </Typography>
-        <ChatPanel />
-      </Container>
+      <MyRuntimeProvider>
+        <Navbar />
+        <Container maxWidth="sm" component="main" sx={{ mt: 2 }}>
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            component="p"
+            sx={{ mb: 1 }}
+          >
+            Chat with an AI bot
+          </Typography>
+          {/* <ChatPanel /> */}
+          <AiChatPanel />
+        </Container>
+      </MyRuntimeProvider>
     </div>
   );
 };
