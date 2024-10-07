40ab20ad04c7d0c4a5cdf9aaedd16cad0bd89833
ikenley <ikenley6@gmail.com>
2024-10-07 19:51:35 -0400

    working draft

--- frontend/src/chat/AiAssistantMessage.tsx
+++ frontend/src/chat/AiAssistantMessage.tsx
@@ -2,8 +2,9 @@ import {
   AssistantActionBar,
   AssistantMessage,
   BranchPicker,
+  useMessageContext,
 } from "@assistant-ui/react";
-import { Avatar } from "@mui/material";
+import { Avatar, Box, CircularProgress } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
 
@@ -12,20 +13,33 @@ import SmartToyIcon from "@mui/icons-material/SmartToy";
  */
 const AiAssistantMessage = () => {
   const theme = useTheme();
+  const messageContext = useMessageContext();
+  const message = messageContext.useMessage();
+  const isLoading = message.status?.type === "running";
 
   return (
     <AssistantMessage.Root>
-      <Avatar
-        sx={{
-          color: "#fff",
-          backgroundColor: theme.palette.primary.main,
-          mr: 1,
-        }}
-      >
-        <SmartToyIcon />
-      </Avatar>
-      {/* <AssistantMessage.Avatar /> */}
-      <AssistantMessage.Content />
+      {isLoading ? (
+        <CircularProgress size="35px" sx={{ mr: 1 }} />
+      ) : (
+        <Avatar
+          sx={{
+            color: "#fff",
+            backgroundColor: theme.palette.primary.main,
+            mr: 1,
+          }}
+        >
+          <SmartToyIcon />
+        </Avatar>
+      )}
+      {isLoading ? (
+        <Box className="aui-assistant-message-content ">
+          <div className="aui-md-root loading-ellipsis">Loading</div>
+        </Box>
+      ) : (
+        <AssistantMessage.Content />
+      )}
+      {/* <AiAssistantMessageContent /> */}
       <BranchPicker />
       <AssistantActionBar />
     </AssistantMessage.Root>
