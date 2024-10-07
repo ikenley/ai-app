e5c6bf3050b408799ef8e979359d0fb83a88da7c
ikenley <ikenley6@gmail.com>
2024-10-07 19:52:01 -0400

    assistant-ui (#29)
    * assistant-ui
    
    * functioning prototype
    
    * light customization
    
    * more style
    
    * working draft

--- frontend/src/chat/ChatPage.tsx
+++ frontend/src/chat/ChatPage.tsx
@@ -1,24 +1,29 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import "@assistant-ui/react/styles/index.css";
 import Navbar from "../shared/Navbar";
-import ChatPanel from "./ChatPanel";
+import { ApiRuntimeProvider } from "./ApiRuntimeProvider";
+import AiChatPanel from "./AiChatPanel";
 
 const ChatPage = () => {
   return (
     <div className="chat-page">
-      <Navbar />
-      <Container maxWidth="sm" component="main" sx={{ mt: 2 }}>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          component="p"
-          sx={{ mb: 1 }}
-        >
-          Chat with an AI bot
-        </Typography>
-        <ChatPanel />
-      </Container>
+      <ApiRuntimeProvider>
+        <Navbar />
+        <Container maxWidth="md" component="main" sx={{ mt: 2 }}>
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            component="p"
+            sx={{ mb: 1 }}
+          >
+            Chat with an AI bot
+          </Typography>
+          {/* <ChatPanel /> */}
+          <AiChatPanel />
+        </Container>
+      </ApiRuntimeProvider>
     </div>
   );
 };
