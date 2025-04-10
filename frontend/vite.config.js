723ae06e79de9b9114d16c91c2797fd674f44f29
Ian Kenley <ikenley6@gmail.com>
2025-04-10 05:26:54 -0400

    fix viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    parallel: true,
-    setupFiles: "./tests/setup.js",
-  },
+export default defineConfig(() => {
+  return {
+    server: {
+      open: true,
+      proxy: {
+        "/ai/api": {
+          target: "http://localhost:8086",
+          changeOrigin: true,
+        },
+        "/auth/api": {
+          target: "http://localhost:8088",
+          changeOrigin: true,
+        },
+      },
+    },
+    // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
+    define: {
+      "process.env": {},
+    },
+    build: {
+      outDir: "build",
+    },
+    plugins: [react(), eslint()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      parallel: true,
+      setupFiles: "./tests/setup.js",
+    },
+  };
 });
